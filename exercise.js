2c6487c8da9c9049e45e6ca1bd7472e68f7fdded
aiman99aleryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-10-12 23:32:38 +0300

    init

---
+++ exercise.js
@@ -0,0 +1,13 @@
+// Do you know how to "merge" the value of the newNumber variable into numberStore without using the array method push?
+
+// let numberStore = [0, 1, 2];
+// let newNumber = 3;
+// numberStore.push(newNumber);
+// console.log(numberStore);
+
+const merge = (numberStore, newNumber) => [...numberStore, newNumber];
+
+let numberStore = [0, 1, 2];
+let newNumber = 3;
+
+console.log(merge(numberStore, newNumber));
